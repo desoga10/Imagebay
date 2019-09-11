278008f28fc41314dfce8758ba2258118599bb7c
desoga <done2stay@gmail.com>
2019-09-11 12:19:27 +0100

    Started working on template design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMasonryModule } from 'ngx-masonry';
+
+
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { MatCardModule, MatInputModule } from '@angular/material';
+import { MasonryModule } from 'angular2-masonry';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,8 +16,18 @@ import { ImageService } from './service/image.service';
 
 @NgModule({
   declarations: [AppComponent, ImagesComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    NgxMasonryModule,
+    BrowserAnimationsModule
+  ],
   providers: [ImageService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
